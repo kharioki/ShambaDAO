4447c7866a0274d13b6396c0666b9fb4d6ea6712
tony <antolisah@gmail.com>
2022-01-06 23:58:38 +0300

    using thirdweb wrapper and connecting to wallet

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import { useEffect, useMemo, useState } from "react";
+import { useWeb3 } from "@3rdweb/hooks";
+
 const App = () => {
+
+  const { connectWallet, address, error, provider } = useWeb3();
+  console.log("👋 Address:", address)
+
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to Shamba DAO</h1>
+        <button onClick={() => connectWallet("injected")} className="btn-hero">
+          Connect your wallet
+        </button>
+      </div>
+    );
+  }
+
+  // When we have the user's address
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
-    </div>
-  );
+      <h1>👀 wallet connected, now what!</h1>
+    </div>);
 };
 
 export default App;
