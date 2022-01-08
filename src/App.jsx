007d94cb87f2872e9da513af91145a20c0c186ad
tony <antolisah@gmail.com>
2022-01-08 23:36:57 +0300

    check if user has membership nft

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,51 @@
 import { useEffect, useMemo, useState } from "react";
 
 // import thirdweb
+import { ThirdwebSDK } from "@3rdweb/sdk";
 import { useWeb3 } from "@3rdweb/hooks";
+
+// instantiate the sdk on Rinkeby.
+const sdk = new ThirdwebSDK("rinkeby");
+
+// reference our ERC-1155 contract.
+const bundleDropModule = sdk.getBundleDropModule(
+  "0xFBE22DB11856ce9960F1B22F2A968aDd907a87FF",
+);
 
 const App = () => {
   // Use the connectWallet hook thirdweb gives us.
   const { connectWallet, address, error, provider } = useWeb3();
   console.log("👋 Address:", address)
 
-  // This is the case where the user hasn't connected their wallet
-  // to your web app. Let them call connectWallet.
+  // user has our NFT.
+  const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
+
+  useEffect(() => {
+    // If they don't have an connected wallet, exit!
+    if (!address) {
+      return;
+    }
+    
+    // Check if the user has the NFT by using bundleDropModule.balanceOf
+    return bundleDropModule
+      .balanceOf(address, "0")
+      .then((balance) => {
+        // If balance is greater than 0, they have our NFT!
+        if (balance.gt(0)) {
+          setHasClaimedNFT(true);
+          console.log("🌟 this user has a membership NFT!")
+        } else {
+          setHasClaimedNFT(false);
+          console.log("😭 this user doesn't have a membership NFT.")
+        }
+      })
+      .catch((error) => {
+        setHasClaimedNFT(false);
+        console.error("failed to nft balance", error);
+      });
+  }, [address]);
+
+  // if user hasn't connected their wallet
   if (!address) {
     return (
       <div className="landing">
@@ -21,8 +57,7 @@ const App = () => {
     );
   }
   
-  // This is the case where we have the user's address
-  // which means they've connected their wallet to our site!
+  // if we have the user's address
   return (
     <div className="landing">
       <h1>👀 wallet connected, now what!</h1>
